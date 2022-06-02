20a2284166b33eac7b406f3b672b9971e061c24e
ReynaldoArias <101991829+ReynaldoArias@users.noreply.github.com>
2022-06-02 17:31:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,61 @@
 # app.py
 from flask import Flask, session, render_template, request, redirect, url_for
-import psycopg2
-import psycopg2.extras
+#import psycopg2
+#import psycopg2.extras
+import sqlite3
 
 app = Flask(__name__)
 
-app.secret_key = "cairocoders-ednalan"
+app.secret_key = "cairocoders"
 
-DB_HOST = "localhost"
-DB_NAME = "sampledb"
-DB_USER = "postgres"
-DB_PASS = "8820"
+#Creacion db
+connection_object = sqlite3.connect("db/tienda_database.db")
+#Cursor de conexion
+cursor_object = connection_object.cursor()
 
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+#DB_HOST = "localhost"
+#DB_NAME = "sampledb"
+#DB_USER = "postgres"
+#DB_PASS = "8820"
+
+#conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 
-@app.route('/')
+@app.route('/carrito')
 def products():
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    #cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    cursor.execute("SELECT * FROM product")
-    rows = cursor.fetchall()
+    cursor_object.execute("SELECT * FROM Articulos")
+    rows = cursor_object.fetchall()
     return render_template('products.html', products=rows)
 
 
-@app.route('/add', methods=['POST'])
+@app.route('/carrito/add', methods=['POST'])
 def add_product_to_cart():
     _quantity = int(request.form['quantity'])
-    _code = request.form['code']
+    _id_articulo = request.form['id_articulo']
     # validate the received values
-    if _quantity and _code and request.method == 'POST':
+    if _quantity and _id_articulo and request.method == 'POST':
 
-        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        #cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-        cursor.execute('SELECT * FROM product WHERE code = %s', (_code,))
-        row = cursor.fetchone()
+        cursor_object.execute('SELECT * FROM Articulos WHERE id_articulo = %s', (_id_articulo,))
+        row = cursor_object.fetchone()
 
         itemArray = {
-            row['code']: {'name': row['name'], 'code': row['code'], 'quantity': _quantity, 'price': row['price'],
-                          'discount': row['discount'], 'image': row['image'],
-                          'total_price': _quantity * (row['price']-row['discount'])}}
+            row['id_articulo']: {'name': row['name'], 'id_articulo': row['id_articulo'], 'quantity': _quantity, 'price': row['price'],
+                          'descuento': row['descuento'], 'image': row['image'],
+                          'total_price': _quantity * (row['price']-row['descuento'])}}
 
         all_total_price = 0
         all_total_quantity = 0
 
         session.modified = True
         if 'cart_item' in session:
-            if row['code'] in session['cart_item']:
+            if row['id_articulo'] in session['cart_item']:
                 for key, value in session['cart_item'].items():
-                    if row['code'] == key:
+                    if row['id_articulo'] == key:
                         old_quantity = session['cart_item'][key]['quantity']
                         total_quantity = old_quantity + _quantity
                         session['cart_item'][key]['quantity'] = total_quantity
@@ -64,17 +71,17 @@ def add_product_to_cart():
         else:
             session['cart_item'] = itemArray
             all_total_quantity = all_total_quantity + _quantity
-            all_total_price = all_total_price + _quantity * ( row['price'] - row['discount'])
+            all_total_price = all_total_price + _quantity * ( row['price'] - row['descuento'])
 
         session['all_total_quantity'] = all_total_quantity
         session['all_total_price'] = all_total_price
 
         return redirect(url_for('products'))
     else:
-        return 'Error while adding item to cart'
+        return 'Error mientras adicionaba item al carrito'
 
 
-@app.route('/empty')
+@app.route('/carrito/empty')
 def empty_cart():
     try:
         session.clear()
@@ -83,15 +90,15 @@ def empty_cart():
         print(e)
 
 
-@app.route('/delete/<string:code>')
-def delete_product(code):
+@app.route('/carrito/delete/<string:code>')
+def delete_product(id_articulo):
     try:
         all_total_price = 0
         all_total_quantity = 0
         session.modified = True
 
         for item in session['cart_item'].items():
-            if item[0] == code:
+            if item[0] == id_articulo:
                 session['cart_item'].pop(item[0], None)
                 if 'cart_item' in session:
                     for key, value in session['cart_item'].items():
@@ -123,4 +130,4 @@ def array_merge(first_array, second_array):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
